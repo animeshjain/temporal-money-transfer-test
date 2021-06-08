407fa822fdbcef3b966c0144782756e872c06ffb
Animesh Jain <jain.kani@gmail.com>
2021-06-08 19:15:50 +0530

    timout increased

--- src/main/java/moneytransferapp/AccountActivityImpl.java
+++ src/main/java/moneytransferapp/AccountActivityImpl.java
@@ -13,6 +13,7 @@ public class AccountActivityImpl implements AccountActivity {
   @Override
   public void deposit(String accountId, String referenceId, double amount) {
     int activityAttempt = Activity.getExecutionContext().getInfo().getAttempt();
+
     if (activityAttempt < 2) {
       try {
         Thread.sleep(10000);
@@ -21,6 +22,7 @@ public class AccountActivityImpl implements AccountActivity {
       }
 //      throw new RuntimeException("simulated");
     }
+
     System.out.printf("*ACTIVITY (attempt %d)* [AccountActivityImpl.deposit] Depositing $%f into account %s. ReferenceId: %s\n", activityAttempt, amount, accountId, referenceId);
   }
 
